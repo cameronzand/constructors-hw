51a9d1060086add6ee4fe88629c92df933d60be1
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-25 20:06:31 -0400

    initial commit

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,8 @@
-function Dog () {
+function Dog (options) {
+	options === options || {};
+	this.color = options.color;
+	this.hungry = options.hungry;
+	this.status = options.happy;
 }
 
 export { Dog };
