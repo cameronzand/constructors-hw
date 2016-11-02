918bf4ffa18be9d533a95a4df65c544185aedde8
Cameron Zandkarimi <camcamcz@gmail.com>
2016-11-02 12:49:13 -0400

    homework

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,8 +1,19 @@
 function Dog (options) {
 	options === options || {};
 	this.color = options.color;
-	this.hungry = options.hungry;
-	this.status = options.happy;
+	this.status = "normal";
+
+	if (options.hungry !== undefined) {
+		this.hungry = options.hungry;
+
+	}
+	else {
+		this.hungry = true;
+	}
+
+
+	// this.hungry = options.hungry !== undefined ? options.hungry : true;
+	// if statement used above also works like above commented out code.
 }
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,16 @@
-function Person () {
+function Person (options) {
+	options = options || {};
+
+
 };
 
+Person.prototype.pet = function(dog) {
+	dog.status = 'happy';
+}
+
+Person.prototype.feed = function(dog) {
+	dog.hungry = false;
+}
+
 export { Person };
+t
